e7186a8bb5afdd1e2a14a7001c1f15fe1cbf1e41
Arezoo Mordai <moradi.arezoo1992@gmail.com>
2024-12-14 22:39:18 +0330

    feat: add nest js microservice

--- admin/src/main.ts
+++ admin/src/main.ts
@@ -1,23 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('api');
-
-  const config = new DocumentBuilder()
-    .setTitle('Even Driven Design RabbitMQ-Admin')
-    .setDescription('Even Driven Design RabbitMQ-Admin')
-    .setVersion('1.0')
-    .addTag('')
-    .build();
-  const documentFactory = () => SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, documentFactory);
-
-  app.enableCors({
-    origin: 'http://localhost:3000',
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'admin_queue',
+      queueOptions: {
+        durable: false,
+      },
+    },
   });
-  await app.listen(8000);
+
+  app.listen();
 }
 bootstrap();
